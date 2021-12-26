d707d6c9855a1292e5dd205869e7ad95776e63cf
nodejk <trynafillmyheart@gmail.com>
2021-12-26 17:00:14 +0100

    inverse kinematics added for first 3 joints

--- kuka.py
+++ kuka.py
@@ -1,11 +1,12 @@
 import numpy as np
 import math
 
+
 def dh_matrix(theta, alpha, r, d):
     """
     This function is used to form the transformation matrix required for the DH table.
     :param theta: angle of rotation around z(i).
-    :param alpha: angle of rotation around x(i+1).
+    :param alpha: angle of rotation around x(i+1) to reach x(i)
     :param r: displacement from i to i+1 frame along the x(i+1) axis.
     :param d: displacement from i to i+1 frame along the z(i) axis.
     :return: 4X4 transformation matrix containing the orientation and the position of the i+1 w.rt. i frame
@@ -14,12 +15,15 @@ def dh_matrix(theta, alpha, r, d):
     theta_ = math.radians(theta)
     alpha_ = math.radians(alpha)
 
-    a = np.matrix([ [math.cos(theta_), -1 * math.sin(theta_) * math.cos(alpha_), math.sin(theta_) * math.sin(alpha_), r * math.cos(theta_)],
-                    [math.sin(theta_), math.cos(theta_) * math.cos(alpha_), -1 * math.cos(theta_) * math.sin(alpha_), r * math.sin(theta_)],
-                    [0               , math.sin(alpha_)                   , math.cos(alpha_)                         , d],
-                    [0               , 0                                  , 0                                        , 1]])
+    a = np.matrix([[math.cos(theta_), -1 * math.sin(theta_) * math.cos(alpha_), math.sin(theta_) * math.sin(alpha_),
+                    r * math.cos(theta_)],
+                   [math.sin(theta_), math.cos(theta_) * math.cos(alpha_), -1 * math.cos(theta_) * math.sin(alpha_),
+                    r * math.sin(theta_)],
+                   [0, math.sin(alpha_), math.cos(alpha_), d],
+                   [0, 0, 0, 1]])
 
     return a
+
 
 def magnitudeVector(x, y, z):
     """
@@ -31,6 +35,7 @@ def magnitudeVector(x, y, z):
     """
     return pow(x * x + y * y + z * z, 0.5)
 
+
 def getHypotenuse(a, b):
     """
     Returns the length of hypotenuse of a right angle triangle having sides a and b.
@@ -41,11 +46,23 @@ def getHypotenuse(a, b):
     return pow(a * a + b * b, 0.5)
 
 
+def getCosineAngle(a, b, c):
+    """
+    this function returns the angle between sides a and b of a triangle.
+    :param a: length of triangle with side a.
+    :param b: another length of triangle with side a.
+    :param c: the side opposite to the angle.
+    :return: angle in degrees.
+    """
+    cos1 = (a * a + b * b - c * c) / (2 * a * b)
+
+    return math.degrees(math.acos(cos1))
+
 
 class kuka:
     """
-    This implementation is for the 6-DOF robot
-    Implementation of Forwards Kinematics, Inverse Kinematics, Point to Point Movement and Synchronous movement for Kuka KR 120 R2700-2.
+    This implementation is for the 6-DOF robot Implementation of Forwards Kinematics, Inverse Kinematics,
+    Point to Point Movement and Synchronous movement for Kuka KR 120 R2700-2.
 
     The DH table for the robot is given as follows:
 
@@ -63,6 +80,7 @@ class kuka:
 
 
     """
+
     def __init__(self):
         """
         This initializes the kuka with all the arms lengths according to the datasheet.
@@ -134,25 +152,171 @@ class kuka:
 
         return [xWrist, yWrist, zWrist]
 
+    def __theta1Configurations(self, xWrist, yWrist, zWrist):
+        """
+        This method returns 3 configurations for the first joint.
+        :param flange: final 4 X 4 matrix of the end effector
+        :return: [theta1.1, theta1.2, theta1.3] angles in degrees.
+        """
+
+        theta1 = math.degrees(math.atan2(yWrist, xWrist))
+
+        if yWrist >= 0 and xWrist < 0:
+            return [180 - theta1, (180 - theta1) + 180, (180 - theta1) - 180]
+        elif yWrist >= 0 and xWrist > 0:
+            return [-theta1, -theta1 - 180, 180 - theta1]
+        elif yWrist <= 0 and xWrist < 0:
+            return [-(180 + theta1), -theta1, -180 - (180 + theta1)]
+        elif yWrist <= 0 and xWrist > 0:
+            return [-theta1, -(theta1 - 180), -(180 + theta1)]
+        elif xWrist == 0:
+            return [90, -90]
+
+    def __getLink2Position(self, theta1):
+        """
+        This method is used to find the position and orientation for the 2nd link.
+        This is important in order to calculate 3rd and 2nd configurations.
+        :param theta1: theta1 angle
+        :return: 4X4 transformation matrix in form of numpy array.
+        """
+
+        a1 = dh_matrix(0, self.alpha0, self.a0, self.d0)
+        a2 = dh_matrix(theta1, self.alpha1, self.a1, self.d1)
+
+        return np.matmul(a1, a2)
+
+    def __theta2Configurations(self, theta1Configurations, xWrist, yWrist, zWrist):
+        """
+        This method returns the 6 (at max) configurations for elbow up and elbow down.
+        :param theta1Configurations: list of theta1 configurations.
+        :param xWrist: x coordinate of the wrist.
+        :param yWrist: y coordinate of the wrist.
+        :param zWrist: z coordinate of the wrist.
+        :return: [[theta1.1, theta2.1, mark], [theta1.1, theta2.2, mark], ...]
+        mark = 1 for elbow up configuration.
+        mark = 0 for elbow down configuration.
+        This mark is used to distinguish the 2 configurations.
+
+        Refer to inverse kinematics for 2 links for additional info.
+        """
+        outputConfigurations = []
+
+        wristCoordinates = np.expand_dims(np.array([xWrist, yWrist, zWrist, 1]), axis=1)
+
+        ld = getHypotenuse(self.d4, self.a3)
+        l2 = abs(self.a2)
+
+        for _, theta1 in enumerate(theta1Configurations):
+
+            link2TransformationMatrix = self.__getLink2Position(theta1)
+
+            link2TranforsmationMatrix = np.linalg.inv(link2TransformationMatrix)
+
+            # get coordinates of the wrist point w.r.t to link2.
+            wristCoordinatesLink2 = np.matmul(link2TranforsmationMatrix, wristCoordinates)
+
+            # distance of wrist point from link2.
+            hypotenuseLength = getHypotenuse(wristCoordinatesLink2[0].item(), wristCoordinatesLink2[1].item())
+
+            beta1 = math.degrees(math.atan2(wristCoordinatesLink2[1], wristCoordinatesLink2[0]))
+
+            # since not every configuration is possible for theta2.
+            try:
+                gamma1 = getCosineAngle(l2, hypotenuseLength, ld)
+
+                elbowUp = beta1 + gamma1
+                elbowDown = beta1 - gamma1
+
+                # this is just angle adjustment according to the project's desired configuration.
+                # found this with experimentation.
+
+                if -elbowDown - 180 > 0:
+                    elbowDown += 360
+                if elbowUp - 180 > 0:
+                    elbowUp -= 360
+
+                outputConfigurations.append([theta1, elbowUp, 1])
+                outputConfigurations.append([theta1, elbowDown, 0])
+
+            except:
+                pass
+
+        return outputConfigurations
+
+    def __theta3Configurations(self, theta2Configurations, xWrist, yWrist, zWrist):
+        """
+        This method is used to find the third configurations.
+        :param theta2Configurations: list of configurations found in method __theta2Configurations.
+        :param xWrist: x coordinate of the wrist.
+        :param yWrist: y coordinate of the wrist.
+        :param zWrist: z coordinate of the wrist.
+        :return: [[theta1.1, theta2.1, theta3.1], [theta1.1, theta2.2, theta3.2], ....]
+        """
+
+        outputConfigurations = []
+
+        wristCoordinates = np.expand_dims(np.array([xWrist, yWrist, zWrist, 1]), axis=1)
+
+        # shoulder offset.
+        phi = math.degrees(math.atan2(self.a3, abs(self.d4)))
+
+        l2 = abs(self.a2)
+        ld = getHypotenuse(self.d4, self.a3)
+
+        for _, thetaConfiguration in enumerate(theta2Configurations):
+            theta1 = thetaConfiguration[0]
+            theta2 = thetaConfiguration[1]
+            elbow = thetaConfiguration[2]
+
+            link2TransformationMatrix = self.__getLink2Position(theta1)
+            link2TransformationMatrix = np.linalg.inv(link2TransformationMatrix)
+
+            # get coordinates of the wrist point w.r.t to link2.
+            wristCoordinatesLink2 = np.matmul(link2TransformationMatrix, wristCoordinates)
+            hypotenuseLength = getHypotenuse(wristCoordinatesLink2[0].item(), wristCoordinatesLink2[1].item())
+
+            try:
+                eta = getCosineAngle(l2, ld, hypotenuseLength)
+
+                if elbow == 1:
+
+                    outputConfigurations.append([theta1, theta2, -180 + phi + eta])
+                else:
+                    theta3 = 180 + phi - eta
+
+                    if theta3 > 180:
+                        theta3 = phi - eta - 180
+
+                    outputConfigurations.append([theta1, theta2, theta3])
+            except:
+                pass
+
+        return outputConfigurations
+
+
     def inverseKinematics(self, endTransformationMatrix):
         """
-        This function is used for the inverse kinematics.
+        This method is used for the inverse kinematics.
         This calculates all the configurations for a given end effector position and orientation
         using geometry.
         Note that these might be all the valid possible configurations for the robot
         because of physical constraints.
         Additionally, this method returns configurations according to the direction of the robot
         defined in the datasheet.
-        :param endTransformationMatrix: 4X4 end transformation matrix.
+        :param endTransformationMatrix: 4X4 end transformation matrix as a form of numpy array.
         :return: [[theta1, theta2,...theta6], [theta1, theta2,...theta6]...]
         """
 
+        xWrist, yWrist, zWrist = self.getWristPosition(endTransformationMatrix)
+
+        theta1Configurations = self.__theta1Configurations(xWrist, yWrist, zWrist)
+
+        theta2Configurations = self.__theta2Configurations(theta1Configurations, xWrist, yWrist, zWrist)
+
+        theta3Configurations = self.__theta3Configurations(theta2Configurations, xWrist, yWrist, zWrist)
+
+        return theta3Configurations
+
+    def line2lineMovement(self, xStart, yStart, zStart, xEnd, yEnd, zEnd):
 
         pass
-
-
-    def point2PointMovement(self, xStart, yStart, zStart, xEnd, yEnd, zEnd):
-
-        pass
-
-
--- main.py
+++ main.py
@@ -2,13 +2,9 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import numpy as np
 
 from kuka import kuka
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    pass
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -17,5 +13,21 @@ if __name__ == '__main__':
     kukaBot = kuka()
 
     # print(kukaBot.forwardKinematics(180, -90, 90, -180, 90, 180))
-    print(kukaBot.forwardKinematics(20, -80, 100, -45, 50, -10))
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # print(kukaBot.forwardKinematics(20, -80, 100, -45, 50, -10))
+
+    # print(kukaBot.forwardKinematics(1.29123907, -100.90206758, 63.85065805, -122.53481659, 101.96362111, -37.67753418))
+
+    # for testing purpose.
+    endEffector = [[ -8.85562315e-01,   2.69213051e-02,  -4.63739830e-01,   9.16922244e+02],
+                   [ -4.47193236e-01,  -3.19530365e-01,   8.35415199e-01,   1.56699319e+02],
+                   [ -1.25688490e-01,   9.47193533e-01,   2.95003075e-01,   2.66453955e+03],
+                   [ 0.00000000e+00, 0.00000000e+00, 0.00000000e+00,  1.00000000e+00]]
+    # print(kukaBot.)
+    endEffector = np.array(endEffector)
+
+    # print(kukaBot.inverseKinematics(endEffector))
+    # temp = kukaBot.inverseKinematics(endEffector)
+
+    # for i in temp:
+    #     print(i)
+
